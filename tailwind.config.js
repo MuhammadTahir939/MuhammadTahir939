ad964c436d718f158a4f114ea2d3194302aa54ac
MuhammadTahir939 <tahirmansha01@gmail.com>
2024-10-02 17:58:13 +0500

    Clone of X

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'], // Ensure you're pointing to the correct HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
